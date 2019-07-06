538747e43f85346410c13392cbd21ddb0316dfb5
juliahop <hop.julia@hotmail.com>
2019-07-07 00:03:01 +0200

    Style mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 function mobileMenu() {
 
-  let mq = window.matchMedia("(max-width: 760px)");
-  let menuItems = document.querySelector(".menu__items.wrapper--flex");
+  let mq = window.matchMedia("(max-width: 47.5em)");
+  let menuItems = document.querySelector(".menu.wrapper--flex");
   let menuButton = document.querySelector(".menu__button");
 
   if (mq.matches) {
@@ -10,32 +10,32 @@ function mobileMenu() {
 
   addListener(menuButton, 'click', function () {
           menuItems.classList.toggle("hidden");
-      });
+  });
 
-      addListener(menuButton, 'keyup', function(event) {
-          if (event.keyCode == 13) {
+  addListener(menuButton, 'keyup', function(event) {
+      if (event.keyCode == 13) {
               menuButton.click();
-          }
-      });
+      }
+  });
 
-      addListener(window, 'resize', function () {
-        var width = window.innerWidth ||
-                    document.documentElement.clientWidth ||
-                    document.body.clientWidth;
+  addListener(window, 'resize', function () {
+      var width = window.innerWidth ||
+                document.documentElement.clientWidth ||
+                document.body.clientWidth;
 
-        if (width > 760) {
-            menuItems.classList.remove("hidden");
-        } else {
-            menuItems.classList.add("hidden");
+      if (width > 760) {
+          menuItems.classList.remove("hidden");
+      } else {
+          menuItems.classList.add("hidden");
+      }
+  });
+
+  function addListener(element, type, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(type, callback);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + type, callback);
         }
-    });
-
-      function addListener(element, type, callback) {
-            if (element.addEventListener) {
-                element.addEventListener(type, callback);
-            } else if (element.attachEvent) {
-                element.attachEvent('on' + type, callback);
-            }
-        }
+    }
 
 };
